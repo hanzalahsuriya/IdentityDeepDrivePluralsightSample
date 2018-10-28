c5b3801e99f98a7d81c133931e37fa05ec3c76de
hanzalahsuriya <hanzalah@gmail.com>
2018-10-28 14:15:05 +0000

    IdentityDeepDrivePluralSightSample

---
+++ PluralsightUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PluralsightDemo
+{
+    public class PluralsightUser : IdentityUser
+    {
+        public string Locale { get; set; } = "en-GB";
+
+        public string OrgId { get; set; }
+    }
+
+    public class Organization
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PluralsightDemo
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            var connectionString =
+                @"Data Source=(LocalDb)\MSSQLLocalDB;database=PluralsightDemo.PluralsightUser;trusted_connection=yes;";
+            var migrationAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+            services.AddDbContext<PluralsightUserDbContext>(opt => opt.UseSqlServer(connectionString,
+                sql => sql.MigrationsAssembly(migrationAssembly)));
+
+            services.AddIdentity<PluralsightUser, IdentityRole>(options =>
+                {
+                    //options.SignIn.RequireConfirmedEmail = true;
+                    options.Tokens.EmailConfirmationTokenProvider = "emailconf";
+
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequiredUniqueChars = 4;
+
+                    options.User.RequireUniqueEmail = true;
+
+                    options.Lockout.AllowedForNewUsers = true;
+                    options.Lockout.MaxFailedAccessAttempts = 3;
+                    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+                })
+                .AddEntityFrameworkStores<PluralsightUserDbContext>()
+                .AddDefaultTokenProviders()
+                .AddTokenProvider<EmailConfirmationTokenProvider<PluralsightUser>>("emailconf")
+                .AddPasswordValidator<DoesNotContainPasswordValidator<PluralsightUser>>();
+
+            services.AddScoped<IUserClaimsPrincipalFactory<PluralsightUser>,
+                PluralsightUserClaimsPrincipalFactory>();
+
+            services.Configure<DataProtectionTokenProviderOptions>(options =>
+                options.TokenLifespan = TimeSpan.FromHours(3));
+            services.Configure<EmailConfirmationTokenProviderOptions>(options =>
+                options.TokenLifespan = TimeSpan.FromDays(2));
+
+            services.ConfigureApplicationCookie(options => options.LoginPath = "/Home/Login");
+
+            services.AddAuthentication()
+                .AddGoogle("google", options =>
+                {
+                    options.ClientId = "249525409876-a2ngkeveqkdsun236221hstj9nrc6pmn.apps.googleusercontent.com";
+                    options.ClientSecret = "2DQ-3qTvq0Ic5LcOhLQ-8mn_";
+                    options.SignInScheme = IdentityConstants.ExternalScheme;
+                });
+        }
+        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+
+            app.UseAuthentication();
+
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
